86ae2f7faf5d03b76845876ab86f737f463dd405
kewton <newtons.boiled.clock@gmail.com>
2024-04-05 23:58:06 +0900

    メンテ

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import {
   UserGroupIcon,
   HomeIcon,
@@ -22,10 +21,11 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname(); // ここでフックを呼び出す
+
   return (
     <>
       {links.map((link) => {
-        const pathname = usePathname();
         const LinkIcon = link.icon;
         return (
           <Link
@@ -34,7 +34,7 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                'bg-sky-100 text-blue-600': pathname === link.href, // マッピング関数内で`pathname`を使用
               },
             )}
           >
